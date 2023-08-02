ba446f46d6eb274b5d6eeca64d04e97c696b7da1
ogikun <g1621362@gmail.com>
2023-08-03 03:16:46 +0900

    added top section

--- src/components/elements/common/SearchBar.jsx
+++ src/components/elements/common/SearchBar.jsx
@@ -1,30 +1,32 @@
 import { MdSearch } from "react-icons/md";
 import { useState } from "react";
 
-const SearchBar = ({ onSubmit }) => {
+const SearchBar = ({ onSubmit, className }) => {
   const [searchWord, setSearchWord] = useState("");
 
   return (
-    <form
-      className="flex h-14 justify-center min-w-min"
-      onSubmit={(event) => {
-        event.preventDefault();
-        onSubmit(searchWord);
-      }}
-    >
-      <input
-        type="text"
-        className="rounded-full mr-4 border min-w-[400px] w-full px-7"
-        value={searchWord}
-        onChange={(event) => setSearchWord(event.target.value)}
-      />
-      <button
-        type="submit"
-        className="rounded-full border p-4 w-32 flex justify-center hover:bg-gray-100"
+    <div className={className}>
+      <form
+        className="flex h-14 justify-center min-w-min"
+        onSubmit={(event) => {
+          event.preventDefault();
+          onSubmit(searchWord);
+        }}
       >
-        <MdSearch className="h-full w-full" />
-      </button>
-    </form>
+        <input
+          type="text"
+          className="rounded-full mr-4 border min-w-[128px] w-full px-7"
+          value={searchWord}
+          onChange={(event) => setSearchWord(event.target.value)}
+        />
+        <button
+          type="submit"
+          className="rounded-full border p-4 w-32 flex justify-center hover:bg-gray-100"
+        >
+          <MdSearch className="h-full w-full" />
+        </button>
+      </form>
+    </div>
   );
 };
 
