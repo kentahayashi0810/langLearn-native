fd99455499808583bfcdd4cc4831b98d0901395a
ogikun <g1621362@gmail.com>
2023-08-11 00:45:16 +0900

    fixed main visual

--- src/components/elements/common/SearchBar.jsx
+++ src/components/elements/common/SearchBar.jsx
@@ -7,23 +7,23 @@ const SearchBar = ({ onSubmit, className }) => {
   return (
     <div className={className}>
       <form
-        className="flex h-14 min-w-min justify-center"
+        className='flex h-14'
         onSubmit={(event) => {
           event.preventDefault();
           onSubmit(searchWord);
         }}
       >
         <input
-          type="text"
-          className="mr-4 w-full min-w-[128px] rounded-full border px-7"
+          type='text'
+          className='mr-4 w-full min-w-[128px] rounded-full border border-black px-7'
           value={searchWord}
           onChange={(event) => setSearchWord(event.target.value)}
         />
         <button
-          type="submit"
-          className="flex w-32 justify-center rounded-full border p-4 hover:bg-gray-100"
+          type='submit'
+          className='flex w-32 justify-center rounded-full border border-black p-4 hover:bg-gray-100'
         >
-          <MdSearch className="h-full w-full" />
+          <MdSearch className='h-full w-full' />
         </button>
       </form>
     </div>
