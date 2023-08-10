cab32736f254b788066f6403c857c302b55479ad
ogikun <g1621362@gmail.com>
2023-08-10 22:12:39 +0900

    format jsx

--- src/components/elements/common/SearchBar.jsx
+++ src/components/elements/common/SearchBar.jsx
@@ -1,13 +1,13 @@
-import { MdSearch } from "react-icons/md";
-import { useState } from "react";
+import { MdSearch } from 'react-icons/md';
+import { useState } from 'react';
 
 const SearchBar = ({ onSubmit, className }) => {
-  const [searchWord, setSearchWord] = useState("");
+  const [searchWord, setSearchWord] = useState('');
 
   return (
     <div className={className}>
       <form
-        className="flex h-14 justify-center min-w-min"
+        className="flex h-14 min-w-min justify-center"
         onSubmit={(event) => {
           event.preventDefault();
           onSubmit(searchWord);
@@ -15,13 +15,13 @@ const SearchBar = ({ onSubmit, className }) => {
       >
         <input
           type="text"
-          className="rounded-full mr-4 border min-w-[128px] w-full px-7"
+          className="mr-4 w-full min-w-[128px] rounded-full border px-7"
           value={searchWord}
           onChange={(event) => setSearchWord(event.target.value)}
         />
         <button
           type="submit"
-          className="rounded-full border p-4 w-32 flex justify-center hover:bg-gray-100"
+          className="flex w-32 justify-center rounded-full border p-4 hover:bg-gray-100"
         >
           <MdSearch className="h-full w-full" />
         </button>
--- src/components/layouts/Header/index.jsx
+++ src/components/layouts/Header/index.jsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import { useState } from "react";
-import Logo from "../../../../public/logo.png";
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { useState } from 'react';
+import Logo from '../../../../public/logo.png';
 
 export const Header = () => {
   const [navOpen, setNavOpen] = useState(false);
@@ -21,53 +21,53 @@ export const Header = () => {
   };
 
   return (
-    <header className="w-full px-6 h-[70px] flex items-center justify-between bg-white drop-shadow-lg fixed top-0 left-0 z-[9999]">
+    <header className="fixed left-0 top-0 z-[9999] flex h-[70px] w-full items-center justify-between bg-white px-6 drop-shadow-lg">
       <Image src={Logo} alt="Lang Learn" className="max-w-[180px]" />
       <div
-        className="w-[100px] h-[100px] flex flex-col justify-center items-center gap-[10px] relative z-20 cursor-pointer"
+        className="relative z-20 flex h-[100px] w-[100px] cursor-pointer flex-col items-center justify-center gap-[10px]"
         onClick={toggleNav}
       >
         <span
           className={
             !navOpen
-              ? "inline-block w-[40px] border-t-4 border-gray-500 rounded-lg transition-all duration-300"
-              : "inline-block w-[40px] border-t-4 border-gray-500 rounded-lg transition-all duration-300 rotate-45 translate-y-[12px]"
+              ? 'inline-block w-[40px] rounded-lg border-t-4 border-gray-500 transition-all duration-300'
+              : 'inline-block w-[40px] translate-y-[12px] rotate-45 rounded-lg border-t-4 border-gray-500 transition-all duration-300'
           }
         ></span>
         <span
           className={
             !navOpen
-              ? "inline-block w-[40px] border-t-4 border-gray-500 rounded-lg transition-all duration-300"
-              : "opacity-0 transition-all duration-300"
+              ? 'inline-block w-[40px] rounded-lg border-t-4 border-gray-500 transition-all duration-300'
+              : 'opacity-0 transition-all duration-300'
           }
         ></span>
         <span
           className={
             !navOpen
-              ? "inline-block w-[40px] border-t-4 border-gray-500 rounded-lg transition-all duration-300"
-              : "inline-block w-[40px] border-t-4 border-gray-500 rounded-lg transition-all duration-300 rotate-[-45deg] translate-y-[-12px]"
+              ? 'inline-block w-[40px] rounded-lg border-t-4 border-gray-500 transition-all duration-300'
+              : 'inline-block w-[40px] translate-y-[-12px] rotate-[-45deg] rounded-lg border-t-4 border-gray-500 transition-all duration-300'
           }
         ></span>
       </div>
       <ul
         className={
           !navOpen
-            ? "w-full h-screen absolute top-0 z-10 right-0 translate-x-[100vw] transition-all duration-300 flex flex-col items-center justify-center bg-gray-300 gap-6 pb-[10%]"
-            : "w-full h-screen absolute top-0 z-10 right-0 translate-x-0 transition-all duration-300 flex flex-col items-center justify-center bg-gray-300 gap-6 pb-[10%]"
+            ? 'absolute right-0 top-0 z-10 flex h-screen w-full translate-x-[100vw] flex-col items-center justify-center gap-6 bg-gray-300 pb-[10%] transition-all duration-300'
+            : 'absolute right-0 top-0 z-10 flex h-screen w-full translate-x-0 flex-col items-center justify-center gap-6 bg-gray-300 pb-[10%] transition-all duration-300'
         }
       >
         <Link href="/" onClick={clickLinkHandler}>
-          <li className="p-3 pb-4 rounded-xl text-3xl font-bold hover:text-5xl hover:bg-white hover:p-5 hover:pb-6 transition-all duration-200 hover:rotate-12 hover:text-orange-400 active:before:content-['/'] active:before:absolute active:before:text-orange-400 active:before:right-0 active:before:top-[-10px] active:before:text-2xl">
+          <li className="rounded-xl p-3 pb-4 text-3xl font-bold transition-all duration-200 hover:rotate-12 hover:bg-white hover:p-5 hover:pb-6 hover:text-5xl hover:text-orange-400 active:before:absolute active:before:right-0 active:before:top-[-10px] active:before:text-2xl active:before:text-orange-400 active:before:content-['/']">
             Home
           </li>
         </Link>
         <Link href="/how-to-use" onClick={clickLinkHandler}>
-          <li className="p-3 pb-4 rounded-xl text-3xl font-bold hover:text-5xl hover:bg-white hover:p-5 hover:pb-6 transition-all duration-200 hover:rotate-[-12deg] hover:text-blue-400">
+          <li className="rounded-xl p-3 pb-4 text-3xl font-bold transition-all duration-200 hover:rotate-[-12deg] hover:bg-white hover:p-5 hover:pb-6 hover:text-5xl hover:text-blue-400">
             How to Use
           </li>
         </Link>
         <Link href="/mypage" onClick={clickLinkHandler}>
-          <li className="p-3 pb-4 rounded-xl text-3xl font-bold hover:text-5xl hover:bg-white hover:p-5 hover:pb-6 transition-all duration-200 hover:rotate-12 hover:text-green-400">
+          <li className="rounded-xl p-3 pb-4 text-3xl font-bold transition-all duration-200 hover:rotate-12 hover:bg-white hover:p-5 hover:pb-6 hover:text-5xl hover:text-green-400">
             My Page
           </li>
         </Link>
